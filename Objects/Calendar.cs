d50b2ce51b4152a2696e92458f5baf8b0e1cc255
Minh Phuong and Tammy Dang <minh+tammy@>
2017-02-16 10:34:08 -0800

    initial setup, create constructor

---
+++ Objects/Calendar.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalendarCounter
+{
+  public class Date
+  {
+    private string _inputDates;
+
+    public Date(string inputDates)
+    {
+      _inputDates = inputDates;
+    }
+  }
+}
